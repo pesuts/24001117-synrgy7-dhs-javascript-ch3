02dbea4caf09d3b660c23779e04ccd367719805d
danielsinaga <daniel.sinaga.ds@gmail.com>
2024-04-18 05:44:09 +0700

    Initial commit: add js scripts

---
+++ scripts/filterCarByAvailability.js
@@ -0,0 +1,18 @@
+function filterCarByAvailability(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Tempat penampungan hasil
+  const result = [];
+
+  // Tulis code-mu disini
+  for (const car of cars) {
+    if (car.available === true) { 
+      result.push(car);
+    }
+  }
+
+  // Rubah code ini dengan array hasil filter berdasarkan availablity
+  return result;
+}
+
